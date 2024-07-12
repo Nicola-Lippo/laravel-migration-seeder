41241b2f7b8ec94b5a7f8678b7fa1814c9e90244
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-12 14:46:56 +0200

    [UPD] creation trains.blade + footer and header

--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,1 +1,10 @@
-<header>Ciao sono l' header condiviso</header>
+<header>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('trains') }}">Trains</a>
+        </li>
+    </ul>
+</header>
---
+++ resources/views/trains.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('main')
+    <main>
+        <div class="container">
+            <h1>TRENI</h1>
+        </div>
+    </main>
+@endsection
